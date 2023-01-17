8f47ce4c01ecc0964864e14d4ad7d875865b3a0d
Shashank Agarwal <shashank1010@gmail.com>
2023-01-17 12:45:04 +0300

    Add tests

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,30 +1,12 @@
-import { memo } from 'react';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import 'react-native';
 
-const styles = StyleSheet.create({
-    example: {
-        height: Dimensions.get('screen').height,
-        width: Dimensions.get('screen').width,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+import { cleanup, render, screen } from '@testing-library/react-native';
+
+import { Example } from './index';
+
+afterEach(cleanup);
+
+it('renders correctly', async () => {
+    render(<Example />);
+    expect(screen.getByText('Hello, we made it!')).toBeTruthy();
 });
-
-const Example = memo(() => {
-    return (
-        <View style={styles.example}>
-            <Text>Hello, we made it!</Text>
-        </View>
-    );
-});
-
-export default function () {
-    return (
-        <SafeAreaProvider>
-            <SafeAreaView>
-                <Example />
-            </SafeAreaView>
-        </SafeAreaProvider>
-    );
-}
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,30 @@
-import { View, Text,  } from "react-native";
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { memo } from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+const styles = StyleSheet.create({
+    example: {
+        height: Dimensions.get('screen').height,
+        width: Dimensions.get('screen').width,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export const Example = memo(() => {
+    return (
+        <View style={styles.example}>
+            <Text>Hello, we made it!</Text>
+        </View>
+    );
+});
 
 export default function () {
-    // eslint-disable-next-line no-console
-    console.log(`
-        Start writing your business logic and export files here.
-    `);
-
-    return <SafeAreaProvider>
-        <SafeAreaView>
-        <View><Text>Hello</Text></View>
-        </SafeAreaView>
-    </SafeAreaProvider>
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView>
+                <Example />
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
 }
